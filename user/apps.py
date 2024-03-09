6cc270830034de45f3c8e82e418f46b792b82b98
abhisheksakibanda <abhisheksakibanda@gmail.com>
2024-03-09 09:15:54 -0500

    add project structure

--- user/apps.py
+++ user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserAppConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_app'
+    name = 'user'
