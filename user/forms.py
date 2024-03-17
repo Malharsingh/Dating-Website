0fbf7d831d582b417d8684315bf98105248c89db
Malharsingh <malharthakur.mt@gmail.com>
2024-03-17 02:57:02 -0400

    profile model and form

--- user/forms.py
+++ user/forms.py
@@ -8,3 +8,15 @@ class SignUpStepOneForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'age']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic', 'first_name', 'last_name', 'banner', 'age', 'sex', 'seeking', 'about', 'city', 'online_status']
