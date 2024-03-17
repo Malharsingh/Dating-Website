c84bd8f8b8d31c52e0e50a0161164c552f3e5681
Malharsingh <malharthakur.mt@gmail.com>
2024-03-17 02:58:41 -0400

    profile view

--- user/views.py
+++ user/views.py
@@ -33,3 +33,42 @@ def signup_one(request):
 		'step_one_form': step_one_form,
 	}
 	return render(request, 'user_app/signup_one.html', context)
+
+
+@login_required
+def user_account(request):
+	profile = Profile.objects.get(pk=request.user.pk)
+	if profile.about:
+		"""Личный аккаунт пользователя, где он может редактировать пользовательские данные"""
+		if request.method == 'POST':
+			u_form = UserUpdateForm(request.POST, instance=request.user)
+			p_form = ProfileUpdateForm(request.POST,
+									   request.FILES,
+									   instance=request.user.profile)
+			if int(request.POST['age']) < 18:
+				return render(request, 'user_app/user_account.html',
+							  {'error': 'Your age must be at least 18 years old'})
+			elif not str(request.POST['age']).isnumeric():
+				return render(request, 'user_app/user_account.html', {'error': 'Uncorrect age field'})
+			else:
+				try:
+					u_form.save()
+					p_form.save()
+					return redirect('user_app:user_account')  # Перенаправление на страницу профиля пользователя
+				except ValueError:
+					return render(request, 'user_app/user_account.html',
+								  {'error': 'Files is too large, requirement is less than 2.5 MB'})
+
+		else:
+			u_form = UserUpdateForm(instance=request.user)
+			p_form = ProfileUpdateForm(instance=request.user.profile)
+
+		context = {
+			'u_form': u_form,
+			'p_form': p_form,
+			'favorites': Favorite.objects.filter(user=request.user).order_by('-saved_date')
+		}
+
+		return render(request, 'user_app/user_account.html', context)
+
+	return redirect('user_app:sign_up_step_three')
