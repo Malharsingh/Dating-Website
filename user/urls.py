c53d63e8894563f447d0952ac314c05bb63dd69d
Dhaval0001 <dhavaltrivedi00.01@gmail.com>
2024-03-17 17:22:00 -0400

    urls

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup_one/', views.signup_one, name='signup_one'),
     path('user_account/', views.user_account, name='user_account'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_user, name='logout_user'),
     path('signup/step_three/', views.sign_up_step_three, name='sign_up_step_three'),
 ]
