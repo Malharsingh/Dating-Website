2d5e878e40bddbfff23bd3eefdb43785a3a3daab
Hemit Patel <hemit58@gmail.com>
2024-03-19 16:58:36 -0400

    add pagination view

--- datingapp/views.py
+++ datingapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.shortcuts import redirect
 
 from userapp.models import Profile
 
@@ -11,3 +12,24 @@ def home(request):
     if profile.about:
         return redirect('dating_app:dating')
     return redirect('user_app:sign_up_step_three')
+
+
+def get_pagination(request, profiles_list, objects_num):
+    """Pagination"""
+    paginator = Paginator(profiles_list, objects_num)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        cards = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        cards = paginator.page(paginator.num_pages)
+    page_range = paginator.get_elided_page_range(number=page)
+
+    context = {
+        'cards': cards,
+        'page_range': page_range
+    }
+    return context
