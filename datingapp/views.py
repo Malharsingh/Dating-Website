a63a4778fa3d754766acb9915000e654a1680d89
abhisheksakibanda <abhisheksakibanda@gmail.com>
2024-03-19 16:52:25 -0400

    implement dating template and home view

--- datingapp/views.py
+++ datingapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from userapp.models import Profile
+
 
 # Create your views here.
+@login_required
+def home(request):
+    profile = Profile.objects.get(pk=request.user.pk)
+    if profile.about:
+        return redirect('dating_app:dating')
+    return redirect('user_app:sign_up_step_three')
