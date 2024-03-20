54b626a184f007268d788b4e6edd1bfed989f11b
hetsigandhi <hetsigandhi@gmail.com>
2024-03-20 12:29:03 -0400

    Update views.py

--- datingapp/views.py
+++ datingapp/views.py
@@ -3,8 +3,46 @@ import random
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.shortcuts import get_object_or_404, redirect, render
-
+from .models import Favorite
 from userapp.models import Profile
+
+
+@login_required
+def dating(request):
+    profile = Profile.objects.get(pk=request.user.pk)
+    if profile.about:
+        """Search for users"""
+        query = request.GET.get("q", default="")
+        sex = request.GET.get('sex', default="ALL")
+        if sex == 'ALL':
+            sex = ['M', 'F']
+
+        profiles_list = Profile.objects.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query), sex__in=sex
+        ).exclude(id=request.user.id)
+
+        context = get_pagination(request, profiles_list, 10)
+        if profiles_list:
+            context.update({'query': f'We found {len(profiles_list)} people with name "{query}"'})
+            context.update({'saved_to_favorite': Favorite.objects.values_list('saved', flat=True)})
+            context.update({'favorites': Favorite.objects.filter(user=request.user).order_by('-saved_date')})
+        else:
+            context.update({'query': f'There are no people with name "{query}"'})
+        return render(request, 'dating_app/dating.html', context)
+    return redirect('user_app:sign_up_step_three')
+
+
+def favorite_add(request, user_id):
+    saved = User.objects.get(id=user_id)
+    favorites = Favorite.objects.filter(user=request.user, saved=saved)
+
+    if not favorites.exists():
+        Favorite.objects.create(user=request.user, saved=saved)
+    else:
+        favorite = favorites.first()
+        favorite.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 @login_required
 def partner_account(request, user_id):
@@ -37,7 +75,7 @@ def get_pagination(request, profiles_list, objects_num):
 
     try:
         cards = paginator.page(page)
-    except (EmptyPage, InvalidPage):
+    except(EmptyPage, InvalidPage):
         cards = paginator.page(paginator.num_pages)
     page_range = paginator.get_elided_page_range(number=page)
 
@@ -50,11 +88,12 @@ def get_pagination(request, profiles_list, objects_num):
 
 def random_card(request):
     profile = Profile.objects.get(pk=request.user.pk)
-    card_list = list(Profile.objects.filter(sex__in=str(profile.seeking)).exclude(id=request.user.id))
+    card_list = list(Profile.objects.filter(sex__in=str(profile.seeking)
+                                            ).exclude(id=request.user.id))
     if card_list:
         random_card = random.sample(card_list, 1)
     else:
         random_card = None
-    return render(request, 'datingapp/random_card.html', {'random_card': random_card,
-                                                          'favorites': Favorite.objects.filter(
-                                                              user=request.user).order_by('-saved_date')})
+    return render(request, 'dating_app/random_card.html', {'random_card': random_card,
+                                                           'favorites': Favorite.objects.filter(
+                                                               user=request.user).order_by('-saved_date')})
