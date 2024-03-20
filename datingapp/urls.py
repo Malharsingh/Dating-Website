9554a890d2737a48d260afef026c2733f421b515
hetsigandhi <hetsigandhi@gmail.com>
2024-03-20 12:29:00 -0400

    Update urls.py

--- datingapp/urls.py
+++ datingapp/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
+from django.conf.urls.i18n import i18n_patterns
 
 app_name = 'datingapp'
 
 urlpatterns = [
+    path('', views.dating, name='dating'),
+    path('favorite/add/<int:user_id>/', views.favorite_add, name='favorite_add'),
     path('random_card/', views.random_card, name='random_card'),
     path('<int:user_id>/', views.partner_account, name='partner_account'),
-
 ]
